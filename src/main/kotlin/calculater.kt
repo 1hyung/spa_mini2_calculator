f71873de4701011720502990044e195c2d082832
1hyung <ryu1hyung@naver.com>
2024-04-23 19:24:51 +0900

    lv1.calculrator.v_last

--- src/main/kotlin/calculater.kt
+++ src/main/kotlin/calculater.kt
@@ -6,17 +6,17 @@ fun main() {
     var num2 = readLine()!!.toDouble()
 //lv1
     if (operator == "+") {
-        println("$num1 + $num2")
+        println("${num1 + num2}")
         // 덧셈일 때
     } else if(operator == "-") {
         // 뺄셈일 때
-        println("$num1 - $num2")
+        println("${num1 - num2}")
     } else if(operator == "*") {
         // 곱셈일 때
-        println("$num1 * $num2")
+        println("${num1 * num2}")
     } else if(operator == "/") {
         // 나눗셈일 때
-        println("$num1 / $num2")
+        println("${num1 / num2}")
     } else {
         // 잘못된 연산자 일 때
         println("잘못된 연산입니다.")
