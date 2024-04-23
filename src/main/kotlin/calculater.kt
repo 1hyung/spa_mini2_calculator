ba0ab3b4a186227915653bd51dded2167639e9cf
1hyung <ryu1hyung@naver.com>
2024-04-23 19:27:28 +0900

    lv2.calculator.v_last+remainder

--- src/main/kotlin/calculater.kt
+++ src/main/kotlin/calculater.kt
@@ -8,15 +8,18 @@ fun main() {
     if (operator == "+") {
         println("${num1 + num2}")
         // 덧셈일 때
-    } else if(operator == "-") {
+    } else if (operator == "-") {
         // 뺄셈일 때
         println("${num1 - num2}")
-    } else if(operator == "*") {
+    } else if (operator == "*") {
         // 곱셈일 때
         println("${num1 * num2}")
-    } else if(operator == "/") {
+    } else if (operator == "/") {
         // 나눗셈일 때
         println("${num1 / num2}")
+    } else if (operator == "%") {
+        // 나머지연산일 때
+        println("${num1 % num2}")
     } else {
         // 잘못된 연산자 일 때
         println("잘못된 연산입니다.")
