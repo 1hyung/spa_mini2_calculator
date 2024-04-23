4579e0b490b79a93cc9e89a59d34a508db5c3854
1hyung <ryu1hyung@naver.com>
2024-04-23 19:48:04 +0900

    lv2.calculator.v_last+remainder change chararray error

--- src/main/kotlin/calculater.kt
+++ src/main/kotlin/calculater.kt
@@ -1,12 +1,15 @@
 package org.example
 
 fun main() {
+    //Int, Double는 숫자라 !! 필요, String은 문자라서 !! 필요 없음
     var num1 = readLine()!!.toDouble()
-    var operator = readLine().toString()
+    var operator = readLine().toCharArray()
     var num2 = readLine()!!.toDouble()
-//lv1
-    if (operator == "+") {
+    //Int로 하면 소수점 계산하려면 Double
+//lv2
+    if (operator == '+') {
         println("${num1 + num2}")
+        //${ }이 뭐지
         // 덧셈일 때
     } else if (operator == "-") {
         // 뺄셈일 때
@@ -24,4 +27,5 @@ fun main() {
         // 잘못된 연산자 일 때
         println("잘못된 연산입니다.")
     }
+    //자동실행이 됐으면 좋겠다!!
 }
