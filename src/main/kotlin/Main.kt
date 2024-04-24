821f9176df6333e97bde69306f2bac4d7c18e9a6
1hyung <ryu1hyung@naver.com>
2024-04-24 17:02:48 +0900

    study: 배고픈 고양이에게 사료 주기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -143,16 +143,38 @@ fun main() {
             i++
         }*/
 
-//전화번호부에서 이름이 “참새”인 사람을 찾을때
-    var infos = arrayOf("꿩", "닭", "참새", "오목눈이", "공작새")
+    /*//전화번호부에서 이름이 “참새”인 사람을 찾을때
+        var infos = arrayOf("꿩", "닭", "참새", "오목눈이", "공작새")
 
-    for (info in infos) {
+        for (info in infos) {
 
-        if (info == "참새") {
-            println("찾았다 참새!")
-            break
-        } else {
-            continue
+            if (info == "참새") {
+                println("찾았다 참새!")
+                break
+
+            } else {
+                println(1)
+                continue
+                println(2)
+            }
+        }*/
+
+    //배고픈 고양이에게 사료 주기
+    var catStatus = false
+    println("고양이는 매우 배고픕니다..")
+    println("사료를 10번 먹여주세요")
+
+
+    // 고양이가 현재 배고프니까(false) 사료 배급가능
+    if (catStatus == false) {
+        for (idx in 0..9) {
+            println("${idx}번째: 냠냠")
         }
+    }
+
+    // 사료를 10번 준 이후의 상태변화
+    catStatus = true
+    if (catStatus == true) {
+        println("배부른 고양이입니다")
     }
 }
