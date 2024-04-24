f0612c9c12c84e2f8254518afb482e93f1eea080
1hyung <ryu1hyung@naver.com>
2024-04-24 17:30:52 +0900

    add class

--- src/main/kotlin/calculater.kt
+++ src/main/kotlin/calculater.kt
@@ -1,39 +1,69 @@
 package org.example
 
+class calculator (var n1:Double, var n2:Double, op:String)
+
 fun main() {
     //Int, Double는 숫자라 !! 필요, String은 문자라서 !! 필요 없음
     //val은 변함없는 값, var은 변화하는 값이라서 var 사용
     //안내 추가
     // readLine()!!이 readln()으로 변경 이유 : 추후 추가 예정
     println("첫번째 값은?")
-    var num1 = readln().toDouble()
+    var n1 = readln().toDouble()
     println("골라골라 +, -, *, /, %")
     //readLine() -> readlnOrNull() 변경 이유 추후 추가 예정
-    var operator = readlnOrNull().toString()
+    var op = readlnOrNull().toString()
     println("두번째 값은?")
-    var num2 = readln().toDouble()
+    var n2 = readln().toDouble()
     //Int로 하면 소수점 계산하려면 Double
 //lv2
     //-> 해봐야지
-    if (operator == "+") {
-        println("${num1 + num2}")
+/*    if (op == "+") {
+        println("${n1 + n2}")
         //${ }이 뭐지
         // 덧셈일 때
-    } else if (operator == "-") {
+    } else if (op == "-") {
         // 뺄셈일 때
-        println("${num1 - num2}")
-    } else if (operator == "*") {
+        println("${n1 - n2}")
+    } else if (op == "*") {
         // 곱셈일 때
-        println("${num1 * num2}")
-    } else if (operator == "/") {
+        println("${n1 * n2}")
+    } else if (op == "/") {
         // 나눗셈일 때
-        println("${num1 / num2}")
-    } else if (operator == "%") {
+        println("${n1 / n2}")
+    } else if (op == "%") {
         // 나머지연산일 때
-        println("${num1 % num2}")
+        println("${n1 % n2}")
     } else {
         // 잘못된 연산자 일 때
         println("잘못된 연산입니다.")
+    }*/
+
+    when (op) {
+        "+" -> {
+            println("${n1 + n2}")
+            //${ }이 뭐지
+            // 덧셈일 때
+        }
+        "-" -> {
+            // 뺄셈일 때
+            println("${n1 - n2}")
+        }
+        "*" -> {
+            // 곱셈일 때
+            println("${n1 * n2}")
+        }
+        "/" -> {
+            // 나눗셈일 때
+            println("${n1 / n2}")
+        }
+        "%" -> {
+            // 나머지연산일 때
+            println("${n1 % n2}")
+        }
+        else -> {
+            // 잘못된 연산자 일 때
+            println("잘못된 연산입니다.")
+        }
     }
     //자동실행이 됐으면 좋겠다!!
 }
