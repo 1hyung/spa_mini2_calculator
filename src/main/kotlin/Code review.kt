7f464a5cb6881cd014b0e59ec0cc3cb952eec621
1hyung <ryu1hyung@naver.com>
2024-04-26 00:04:22 +0900

    When문으로 계산분기점을 나누고 함수로 변경했다.

--- src/main/kotlin/Code review.kt
+++ src/main/kotlin/Code review.kt
@@ -8,51 +8,82 @@ fun main() {
     println("두번째 값은?")
     var n2 = readln().toDouble()
 
-/*    val calculator = calculator(n1, n2)
-    calculator.add()
-    calculator.Substract()
-    calculator.multiply()
-    calculator.Divide()
-    calculator.remain()*/
+    val caculater = calculator(n1, n2)
+    when (op) {
+        "+" -> {
+            caculater.Add()
+            println(caculater.Add())
+        }
 
+        "-" -> {
+            caculater.Sub()
+            println(caculater.Sub())
+        }
 
-    //람다 표현식으로 변경
-    val Add: (n1: Double, b: Double) -> Double = { a, b -> a + b }
-    println(Add(n1, n2))
-    val Substract: (n1: Double, b: Double) -> Double = { a, b -> a - b }
-    println(Substract(n1, n2))
-    val multiply: (n1: Double, b: Double) -> Double = { a, b -> a * b }
-    println(multiply(n1, n2))
-    val Divide: (n1: Double, b: Double) -> Double = { a, b -> a / b }
-    println(Divide(n1, n2))
-    val remain: (n1: Double, b: Double) -> Double = { a, b -> a % b }
-    println(remain(n1, n2))
+        "*" -> {
+            caculater.Multiply()
+            println(caculater.Multiply())
+        }
+
+        "/" -> {
+            caculater.Divide()
+            println(caculater.Divide())
+        }
+
+        "%" -> {
+            caculater.remain()
+            println(caculater.remain())
+        }
+
+        else -> println("잘못된 입력입니다. 입력을 확인해주세요.")
+    }
+
+    /*        calculator.Add()
+              calculator.Substract()
+            calculator.multiply()
+            calculator.Divide()
+            calculator.remain()*/
+//클래스의 존재 이유 속성 담아주는것을 안쓰고 있다
 
 }
 
-//class는 고유의 특징값 "속성"과 기능의 구현인 "함수"로 이루어져 있다. 아직 함수는 구현 못함
-class calculator(var n1: Double, var n2: Double, op: String) {
-    // 실제 기능을 작동할 함수 fun 어쩌구 저쩌구}
-    fun add(n1: Double, b: Double): Double {
-        return n1 + n2
-    }
+//class는 고유의 특징값 "속성"과 기능의 구현인 "함수"로 이루어져 있다. 아직 함수는 구현
+class calculator(var n1: Double, var n2: Double) {
+    // 실제 기능을 작동할 함수 fun 어쩌구 저쩌구} 3룸 아파트(필드)
+    /*
+        fun add(n1: Double, b: Double): Double {
+            return n1 + n2
+        }
 
-    fun Substract(n1: Double, b: Double): Double {
-        return n1 + n2
-    }
+        fun Substract(n1: Double, b: Double): Double {
+            return n1 + n2
+        }
 
-    fun multiply(n1: Double, b: Double): Double {
-        return n1 + n2
-    }
+        fun multiply(n1: Double, b: Double): Double {
+            return n1 + n2
+        }
 
-    fun Divide(n1: Double, b: Double): Double {
-        return n1 + n2
-    }
+        fun Divide(n1: Double, b: Double): Double {
+            return n1 + n2
+        }
 
-    fun remain(n1: Double, b: Double): Double {
-        return n1 + n2
-    }
+        fun remain(n1: Double, b: Double): Double {
+            return n1 + n2
+        }
+    */
 
+
+    //람다 표현식으로 변경 class로 이동 fun으로 / 변경 -> : 변경 / 익명함수를 함수로 바꾸는 과정
+    fun Add(): Double = n1 + n2
+    fun Sub(): Double = n1 - n2
+    fun Multiply(): Double = n1 * n2
+    fun Divide(): Double = n1 / n2
+    fun remain(): Double = n1 % n2
+    /*    fun Add2(a: Double, b: Double) : Double { return a + b }
+        fun Substract (a: Double, b: Double) : Double = { a, b -> a - b }
+        fun multiply (a: Double, b: Double) : Double = { a, b -> a * b }
+        fun Divide (a: Double, b: Double) : Double = { a, b -> a / b }
+        fun remain (n1: Double, b: Double) : Double = { a, b -> a % b }*/
 }
 
 
