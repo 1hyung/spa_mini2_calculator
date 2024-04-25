e503927f25167cbf31783a51558b8f937da951f8
1hyung <ryu1hyung@naver.com>
2024-04-26 00:20:14 +0900

    study: collection 변화

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -72,15 +72,44 @@ fun main() { //key의 형태는 string이다. value의 형태는 int다.
     val appleCount = fruitCounts["apple"]  // 5
 }*/
 
+/*
 fun main() { //람다 표현식{a와 b를 받아서 -> 곱한 값을 반환하는 함수}을 multiply라는 변수에 넣는다.
     //형태는 int 값 2개를 받아서 int를 반환하는 이름 없는 함수가 multifly라는 변수 안에 들어간다.
-    val multiply: (Int, Int) -> Int = { a, b -> a * b }
+    //람다 표현식은 { }가 끝 이 안에 함수가 동작해야하는 것들을 써줄 것임. 받을 값 두개 a, b 입력, 매개변수에 타입 입력해줘도 됨!
+    //함수 만들 때 매개변수 사용한 것이랑 같음, 함수의 본문을 쓰기 위해 ->
+    //a, b 값 2개를 받아서 *한 다음 반환 하는 함수가 만들어 진 것
+    val multiply: (a: Int, b: Int) -> Int = { a, b -> a * b }
+    println(multiply(3, 4))
+
     val result = multiply(5, 3) // 결과: 15
     println(result)
 
 
-/*    val numbers = listOf(1, 2, 3, 4, 5)
+    val numbers = listOf(1, 2, 3, 4, 5)
 // forEach는 컬렉션의 각 요소에 대해 주어진 람다 표현식을 실행합니다.
+    //forEach 고차함수 람다 표현식 하나를 받아야 함
+    //value 하나를 받아서 출력하는 람다표현식 그것을 forEach에게 전달하는 예제 코드
+//1
     val printFunction: (Int) -> Unit = { value -> println(value) }
-    numbers.forEach { printFunction }*/
+    numbers.forEach { printFunction }
+//2
+    numbers.forEach { value -> println(value) }
+    //3 기본적으로 it이라는 이름으로 받아오기 때문에 똑같은 과정이 시작됨
+    numbers.forEach { println(it) }
+//forEach는 람다표현식을 받아야 하는 고차함수고 여러방식 람다표현식을 만든 다음에 전달해줘서 리스트에 forEach 반복문을 썼다.
+}
+*/
+//collection 변화
+fun main() {
+    val list = listOf("apple", "banana", "orange")
+    println(list)
+
+    val mapFromList = list.associateWith { it.length }
+    println(mapFromList)
+
+    val map = mapOf("apple" to 5, "banana" to 3, "orange" to 8)
+    println(map)
+
+    val listFromMap = map.toList()
+    println(listFromMap)
 }
